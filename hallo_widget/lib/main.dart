70d33adebe8966ce31e7ad9b7cbdfcecffea2cb9
Agnes Anastasia Putri <agnesanastasiaputri_2226250013@mhs.mdp.ac.id>
2023-10-16 17:54:12 +0700

    Latihan

--- hallo_widget/lib/main.dart
+++ hallo_widget/lib/main.dart
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: "Hallo Widget",
-        theme: ThemeData(primarySwatch: Colors.red),
-        home: FirstScreen(),
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: WidgetDemo(),
         // home: Scaffold(
         //   appBar: AppBar(
         //     title: const Text("Agnes Anastasia Putri"),
@@ -52,6 +52,65 @@ class FirstScreen extends StatelessWidget {
       ),
       body: const Center(
         child: Text("Hallo Agnes Anastasia Putri"),
+      ),
+    );
+  }
+}
+
+class WidgetDemo extends StatelessWidget {
+  const WidgetDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Widget Demo')
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
+            color: Colors.blue,
+            child: const Center(
+              child: Text(
+                'Ini adalah contoh penggunaan Container',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: (){},
+            child: const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text('Tombol Elevated'),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.star, color: Colors.amber),
+              Text('Rating: 4.5'),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.network(
+              'https://picsum.photos/id/7/300/200',
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
       ),
     );
   }
