36a9658f5d4986c33e98f140053dfde3783c4ded
Agnes Anastasia Putri <agnesanastasiaputri_2226250013@mhs.mdp.ac.id>
2023-11-29 22:15:16 +0700

    .

--- cryptotext/test/widget_test.dart
+++ cryptotext/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:cryptotext/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CryptoText());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
